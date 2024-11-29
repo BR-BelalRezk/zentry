aea2fc6e71b7146ed8f275270e9d317da6f30c01
BR-BelalRezk <155649129+BR-BelalRezk@users.noreply.github.com>
2024-11-29 21:28:47 +0200

    Update Videos.tsx

--- src/components/ui/Videos.tsx
+++ src/components/ui/Videos.tsx
@@ -1,40 +1,37 @@
 "use client";
-
+import { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import { useRef, useState } from "react";
-import { VideoPreview } from "./VideoPreview";
-
-gsap.registerPlugin(ScrollTrigger);
+import VideoPreview from "./VideoPreview";
 
 export default function Videos() {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
 
-  const totalVideos = 4;
-  const upcomingVideo = (currentIndex % totalVideos) + 1;
-  const nextVdRef = useRef<HTMLVideoElement>(null);
+  const nxtVidRef = useRef<HTMLVideoElement>(null);
 
-  const handleMiniVdClick = () => {
+  const totalVids = 4;
+  const upcomingVidIndex = (currentIndex % totalVids) + 1;
+  const handleMiniVidClick = () => {
     setHasClicked(true);
-    setCurrentIndex(upcomingVideo);
+    setCurrentIndex(upcomingVidIndex);
   };
+  const getVidSrc = (index: number) => `/videos/hero-${index}.mp4`;
 
   useGSAP(
     () => {
       if (hasClicked) {
-        gsap.set("#next-video", { visibility: "visible" });
-        gsap.to("#next-video", {
+        gsap.set("#nxt-vid", { visibility: "visible" });
+        gsap.to("#nxt-vid", {
           transformOrigin: "center center",
           scale: 1,
           width: "100%",
           height: "100%",
           duration: 1,
           ease: "power1.inOut",
-          onStart: () => nextVdRef?.current?.play() as void,
+          onStart: () => nxtVidRef?.current?.play?.() as void,
         });
-        gsap.from("#current-video", {
+        gsap.from("#current-vid", {
           transformOrigin: "center center",
           scale: 0,
           duration: 1.5,
@@ -42,44 +39,43 @@ export default function Videos() {
         });
       }
     },
-    {
-      dependencies: [currentIndex],
-      revertOnUpdate: true,
-    }
+    { dependencies: [currentIndex], revertOnUpdate: true }
   );
 
-  const getVideoSrc = (index: number) => `videos/hero-${index}.mp4`;
-
   return (
-    <>
-      <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-        <VideoPreview onClick={handleMiniVdClick}>
-          <video
-            ref={nextVdRef}
-            src={getVideoSrc(currentIndex)}
-            loop
-            muted
-            id="current-video"
-            className="size-64 origin-center scale-150 object-cover object-center"
-          />
+    <div>
+      <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+        <VideoPreview>
+          <div
+            onClick={handleMiniVidClick}
+            className=" origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+          >
+            <video
+              className="size-64 origin-center scale-150 object-center object-cover"
+              id="current-vid"
+              loop
+              muted
+              src={getVidSrc(upcomingVidIndex)}
+              ref={nxtVidRef}
+            />
+          </div>
         </VideoPreview>
       </div>
-
       <video
-        ref={nextVdRef}
-        src={getVideoSrc(currentIndex)}
+        className=" absolute-center invisible z-20 size-64 object-cover object-center"
+        id="nxt-vid"
+        ref={nxtVidRef}
+        src={getVidSrc(currentIndex)}
         loop
         muted
-        id="next-video"
-        className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
       />
       <video
-        src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
         autoPlay
         loop
         muted
-        className="absolute left-0 top-0 size-full object-cover object-center"
+        className=" absolute left-0 top-0 size-full object-cover object-center"
+        src={getVidSrc(currentIndex === totalVids - 1 ? 1 : currentIndex)}
       />
-    </>
+    </div>
   );
 }
