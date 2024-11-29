a69fa9a1d54f663a8ec2b728a5e655f6dd32c431
BR-BelalRezk <155649129+BR-BelalRezk@users.noreply.github.com>
2024-11-29 21:29:37 +0200

    Update VideoFrame.tsx

--- src/components/ui/VideoFrame.tsx
+++ src/components/ui/VideoFrame.tsx
@@ -2,6 +2,9 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function VideoFrame({
   children,
@@ -9,27 +12,26 @@ export default function VideoFrame({
   children: React.ReactNode;
 }) {
   useGSAP(() => {
-    gsap.set("#video-frame", {
-      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+    gsap.set("#vid-frame", {
+      clipPath: "polygon(14% 0px, 72% 0px, 88% 90%, 0px 95%)",
       borderRadius: "0% 0% 40% 10%",
     });
-    gsap.from("#video-frame", {
-      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-      borderRadius: "0% 0% 0% 0%",
+    gsap.from("#vid-frame", {
+      clipPath: "polygon(0% 0px, 100% 0px, 100% 100%, 0px 100%)",
+      borderRadius: "0 0 0 0",
       ease: "power1.inOut",
       scrollTrigger: {
-        trigger: "#video-frame",
+        trigger: "#vid-frame",
         start: "center center",
         end: "bottom center",
         scrub: true,
       },
     });
   });
-
   return (
     <div
-      id="video-frame"
-      className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      id="vid-frame"
+      className=" relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
     >
       {children}
     </div>
